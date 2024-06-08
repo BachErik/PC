cb2b341f20c964cb17c5cb7f5b8f33ae260b5164
BachErik <bacherik.info@gmail.com>
2024-06-08 21:33:21 +0200

    fix installation

--- install.sh
+++ install.sh
@@ -1,19 +1,10 @@
 # !/bin/sh
 sudo pacman -Syu
 sudo pacman -S extra/flatpak
+
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
 wget https://raw.githubusercontent.com/BachErik/PC/main/part-2.sh -P /home/$USER/tmp
-sudo echo <<EOF
-[Unit]
-Description=Starts part two of the installation
-
-[Service]
-Type=simple
-User=$USER
-ExecStart=/usr/bin/konsole -e /home/$USER/tmp/part-2.sh
-
-[Install]
-WantedBy=graphical.target
-EOF > /etc/systemd/system/part-2.service
+sudo wget https://raw.githubusercontent.com/BachErik/PC/main/part-2.service -P /etc/systemd/system
 sudo systemctl enable part-2.service
 sudo reboot now
