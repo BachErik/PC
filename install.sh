94df3b8dfc8f7bcb9bf8eca8aa38bd5084038b48
BachErik <bacherik.info@gmail.com>
2024-06-08 21:25:27 +0200

    fix pacman install

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 # !/bin/sh
 sudo pacman -Syu
-sudo pacman -S flatpak
+sudo pacman -S extra/flatpak
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 wget https://raw.githubusercontent.com/BachErik/PC/main/part-2.sh -P /home/$USER/tmp
 sudo echo <<EOF
